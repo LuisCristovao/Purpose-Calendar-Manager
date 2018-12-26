220d92c9921b1bd67965339d105e935f6e621226
LuisCristovao <luis.cristovao92@gmail.com>
2018-12-26 17:32:57 +0000

    insert code

---
+++ PurposeCalendar.js
@@ -0,0 +1,326 @@
+var html=document.getElementById("html");
+var global_width=html.offsetWidth;
+var global_height=html.offsetHeight;
+var prev_width=global_width;
+var prev_height=global_height;
+var body=document.getElementById("body");
+//var purpose=document.title;
+//purpose="Ola"
+
+var months=["Jan","Fev","Mar","Apr","May","Jun","Jul","Ago","Set","Out","Nov","Dez"]
+var days={1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:30,9:31,10:31,11:30,12:31}
+var lines=31
+var columns=12
+
+class LocalStorageManager{
+    
+    constructor(){
+        this.calendar_purpose=this.getTitle()
+        this.JsonData=this.getData()
+        localStorage[this.calendar_purpose]=JSON.stringify(this.JsonData)
+    }
+    
+    getTitle(){
+        if(window.location.search!=""){
+            var p=window.location.search;
+            p=p.substr(1,p.length)
+            document.title=p
+            return p
+        }
+        else{
+            return document.title;
+        }
+        
+    }
+    
+    createDays(month){
+        var d={}
+        for(var i=1;i<=days[month];i++){
+            d[i]=false
+        }
+        
+        return d
+    }
+    
+    createData(){
+        var out=[]
+        
+        for(var i=0;i<months.length;i++)
+        {
+            var month={
+                "month_number":i+1,
+                "month_name":months[i],
+                "days":this.createDays(i+1)
+            }
+            out.push(month)
+        }
+        
+        
+        return out
+    }
+    
+    getData(){
+        //if data already exists
+        if(localStorage[this.calendar_purpose]!=null){
+            return JSON.parse(localStorage[this.calendar_purpose])
+        }
+        else{
+            //localStorage[this.calendar_purpose]=this.createData()
+            return this.createData()
+        }
+    }
+    
+    setDay(month,day,val){
+        this.JsonData[month-1].days[day]=val
+        localStorage[this.calendar_purpose]=JSON.stringify(this.JsonData)
+    }
+    
+    getDay(month,day){
+        return this.JsonData[month-1].days[day]
+    }
+    
+}
+
+
+
+
+
+
+
+
+function columnWidth(){
+    return global_width*((global_height>global_width)?0.5:0.1)
+}
+function headerHeight(){
+    return global_height*0.1
+}
+
+function columnHeight(){
+    return global_height*((global_height>global_width)?0.25:0.35)
+}
+
+function setStyle(element,atributes){
+    var properties=""
+    for(key in atributes){
+        properties+=key+":"+atributes[key]+";"
+    }
+    element.setAttribute("style",properties)        
+}
+
+function CreateDiv(left,top,width,height,color,position){
+    var div=document.createElement("div");
+    var body=document.getElementById("body");
+    var atributes={"top":top,
+                   "left":left,
+                   "width":width,
+                   "height":height,
+                   //"background":color,
+                   "position":position
+                  }
+    setStyle(div,atributes)
+    
+    return div
+    //body.appendChild(hex)
+}
+
+function btnChangeColor(btn){
+    if(btn.getAttribute("isActive")=="true"){
+        btn.style.background="hsl(0,100%,100%)"
+    }
+    else{
+        var color=btn.style.border.split(" ")[2]+btn.style.border.split(" ")[3]+btn.style.border.split(" ")[4]
+        btn.style.background=color
+    }
+}
+
+function btnClick(btn){
+    
+    var d,m;
+    d=btn.getAttribute("id").split("/")[0]
+    m=btn.getAttribute("id").split("/")[1]
+    
+    if(btn.getAttribute("isActive")=="false"){
+        btn.style.background="hsl(0,100%,100%)"
+        btn.setAttribute("isActive","true")
+        lsm.setDay(m,d,true)
+        
+    }else{
+        var color=btn.style.border.split(" ")[2]+btn.style.border.split(" ")[3]+btn.style.border.split(" ")[4]
+        btn.style.background=color
+        btn.setAttribute("isActive","false")
+        lsm.setDay(m,d,false)
+    }
+}
+
+
+
+function CreateButton(_text,_color,_month){
+    
+    var btn=document.createElement("div");
+    //btn.setAttribute("valign","center")
+    var width_height=80;
+    var middle=(100-width_height)/2;
+    
+    setStyle(btn,{"position":"inherit","background":"hsla("+_color+",100%,60%,1)","border-radius":"50%","width":width_height+"%","height":width_height+"%","top":middle+"%","left":middle+"%","border":"5px solid hsla("+_color+",100%,60%,1)","cursor":"pointer"})
+    
+    var text=document.createElement("p")
+    
+    text.appendChild(document.createTextNode(_text))
+    
+    
+    btn.appendChild(text)
+    if(global_height>global_width){
+        setStyle(text,{"text-align":"center","position":"relative","font-size":"12vw","top":"0%"})
+    }
+    else{
+        setStyle(text,{"text-align":"center","position":"relative","font-size":"2.5vw","top":"0%"})
+    }
+    
+    
+    
+    btn.setAttribute("id",_text+"/"+(_month+1));
+    btn.setAttribute("isActive",lsm.getDay(_month+1,_text))
+    btn.setAttribute("onclick","btnClick(this)")
+    btnChangeColor(btn)
+    //btn.addEventListener("click",btnClick(btn))
+    
+    return btn
+}
+
+function CreateHeader(){
+    var body=document.getElementById("body");
+    body.innerHTML="";
+    var header=CreateDiv(0,0,global_width,headerHeight(),"rgb(255,255,255)","absolute")
+    for(var j=0;j<columns;j++){
+        
+        var headerblock=CreateDiv(j*columnWidth(),0,columnWidth(),headerHeight(),"rgb(255,255,255)","absolute")
+        var h1=document.createElement("h1")
+        h1.appendChild(document.createTextNode(months[j]))
+        headerblock.appendChild(h1)
+        if(global_height>global_width){
+                h1.setAttribute("style","font-size:15vw;margin-top:0px;")
+        }
+        else{
+                h1.setAttribute("style","font-size:4vw;margin-top:0px;")
+        }
+
+        headerblock.setAttribute("align","center")
+        header.appendChild(headerblock)
+        
+    }
+    
+    body.appendChild(header)
+}
+
+
+function CreateColumns(lines, columns){
+    var body=document.getElementById("body");
+    
+    var table=CreateDiv(0,headerHeight(),global_width,global_height,"rgb(255,255,255)","absolute")
+    
+    var width_factor=columnWidth()
+    var height_factor=columnWidth()
+    
+    
+    var lines_elements=[];
+    for(var i=0;i<lines;i++){
+        
+        for(var j=0;j<columns;j++){
+            var column=CreateDiv(j*width_factor,i*height_factor,width_factor,height_factor,"rgba("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+","+Math.random()+")","absolute")
+            
+            column.setAttribute("align","center")
+            
+            if(i<days[j+1]){
+                var btn=CreateButton(i+1,j*20,j)
+                column.appendChild(btn)
+            }
+            
+            //line.appendChild(column)
+            table.appendChild(column)
+        }
+    }
+    
+    body.appendChild(table)
+}
+
+function highLightDay(index){
+    var date=new Date()
+    var d=date.getDate()
+    var m=date.getMonth()+1
+    
+    var id=d+"/"+m
+    var btn=document.getElementById(id)
+    
+    if(btn.getAttribute("isActive")=="false"){
+        animation(btn,index)
+    }
+   
+    
+}
+
+function animation(btn,index){
+    btn.style.background="hsla("+index%360+",100%,60%,1)"
+    var color=btn.style.border.split(" ")[2]+btn.style.border.split(" ")[3]+btn.style.border.split(" ")[4]
+    //btn.style.border=(index%10).toString()+"px "+"solid "+"hsl("+color+",100%,60%)"; 
+    //var w=parseInt(btn.style.width.substr(0,btn.style.width.length-1))  
+    //btn.style.width=(w+(index%10))+"%";
+}
+
+
+window.onload=function(){
+    //alert("hello");
+    
+    
+    
+    
+    global_width=html.offsetWidth;
+    global_height=html.offsetHeight;
+    prev_width=global_width;
+    prev_height=global_height;
+    
+    CreateHeader()    
+    CreateColumns(lines,columns)
+    
+    //setStyle(body,{"width":global_width,"height":global_height,"top":"0px","left":"0px","position":"absolute"})
+    var date=new Date()
+    var d=date.getDate()
+    var m=date.getMonth()+1
+    
+    var id=d+"/"+m
+     //go to actual button in page
+    window.location.href="#"+id
+    requestAnimationFrame(Main);
+}
+
+//Main-------------------
+
+
+var lsm=new LocalStorageManager();
+var index=0;
+
+function Main(){
+    var html=document.getElementById("body");
+    global_width=html.offsetWidth;
+    global_height=html.offsetHeight;
+    //console.log("with: "+global_width)
+    //console.log("height: "+global_height)
+    if(prev_width!=global_width || prev_height!=global_height){
+        console.log("change")
+        prev_height=global_height
+        prev_width=global_width
+        
+        CreateHeader()
+        CreateColumns(lines,columns)
+        
+        //var body=document.getElementById("html");
+        //setStyle(body,{"width":global_width,"height":global_height,"top":"0px","left":"0px","position":"absolute"})
+    }
+    
+    //Highlight actual day
+    
+    highLightDay(index)
+    index=(index+10)%1000;
+    
+    //setTimeout(Main,100);
+    requestAnimationFrame(Main)
+}
---
+++ PurposeCalendarManager.js
@@ -0,0 +1,424 @@
+
+var html=document.getElementsByTagName("html")[0];
+var global_width=html.offsetWidth;
+var global_height=html.offsetHeight;
+var prev_width=global_width;
+var prev_height=global_height;
+
+
+
+
+var all_calendars=getCalendars()
+
+
+function getCalendars(){
+    if(localStorage["PCM"]!=null){
+        CalculateDaysSelected()
+        return JSON.parse(localStorage["PCM"])
+    }
+    else{
+        localStorage["PCM"]=JSON.stringify({})
+        return {}
+    }
+}    
+function CalculateDaysSelected(){
+    var pcm=JSON.parse(localStorage["PCM"])
+    for(var calendar in pcm){
+        var days_selected=0;
+        if(localStorage[calendar]!=null){
+            
+            var cal_json=JSON.parse(localStorage[calendar])
+            for(var month_index=0;month_index<cal_json.length;month_index++){
+                var days=cal_json[month_index].days
+                for(var day in days){
+                    if(days[day]==true){
+                        days_selected++
+                    }
+                } 
+            }
+        }
+        pcm[calendar]="("+days_selected+"/365)"
+    }
+    localStorage["PCM"]=JSON.stringify(pcm)
+}  
+    
+function range(initial,end){
+    var array=[]
+    for(var i=initial;i<end;i++){
+        array.push(i)
+    }
+    return array
+}
+
+function Highlight(el){
+    el.style["text-decoration"]="underline"
+}
+function NotHighLight(el){
+    el.style["text-decoration"]=""
+}
+
+function titleSize(){
+    return (global_height>global_width)?"10vh":"7vw"
+}
+function tableFontSize(){
+    return (global_height>global_width)?"6vh":"8vh"
+}
+function inputFontSize(){
+    return (global_height>global_width)?"6vw":"6vh"
+}
+function tablePadding(){
+    return (global_height>global_width)?"5%":"0%"
+}
+function SubMenuFontSize(){
+    return (global_height>global_width)?"5vh":"8vh"
+}
+
+function hyperlinkStyle(color){
+    return "text-decoration:underline;color:"+color+";"
+}
+
+function updatePrevWidthHeight(){
+     //update prev_width and height to not use createPage()
+    html=document.getElementsByTagName("html")[0];
+    global_width=html.offsetWidth;
+    global_height=html.offsetHeight;
+    prev_width=global_width;
+    prev_height=global_height;
+}
+
+function removeSubMenu(el){
+    //el.get
+    //CreatePage()
+    
+    var new_html='<td><ul><li onmouseover="Highlight(this)" onmouseout="NotHighLight(this)" was_clicked="false" onclick="clickAction(this)" style="cursor:pointer;">'+el.innerText.split("\n")[0]+"</li></ul></td>"
+    new_html+="<td><ul>"+all_calendars[el.innerText.split("\n")[0]]+"</ul></td>"
+    //changing row
+    el.parentElement.parentElement.innerHTML=new_html
+    
+}
+function addSubMenu(el){
+    
+    //el.parentElement.parentElement.innerHTML="<td colspan='2'><ul><li>ola</li></ul></td>"
+    
+    //change array to map of option_name:function_to_execute()
+    var map={"Enter Calendar":"enterCalendar(this)","Import Calendar":"importCalendar(this)","Export Calendar":"exportCalendar(this)","Reset Calendar":"resetCalendar(this)","Remove Calendar":"removeCalendar(this)"}
+    
+    var new_html="<td colspan='2'><ul>"+el.innerHTML.split("</ul")[0]+"<div>"
+
+    for(var key in map){
+        new_html+='<ul onclick="'+map[key]+'"><li style="'+hyperlinkStyle('#0183D9')+';cursor:pointer;font-size:'+SubMenuFontSize()+'">'+key+'</li></ul>'
+    }
+    new_html+="</ul></div></td>"
+    //changing row
+    el.parentElement.parentElement.innerHTML=new_html
+    
+    /*var ul=document.createElement("ul")
+    var li=document.createElement("li")
+    
+    ul.appendChild(li)
+    el.appendChild(ul)
+    
+    li*/
+}
+
+
+function enterCalendar(el){
+    var calendar=el.parentElement.parentElement.innerText.split("\n")[0];
+    window.location.href="PurposeCalendar.html?"+calendar
+}
+function resetCalendar(el){
+    var calendar=el.parentElement.parentElement.innerText.split("\n")[0];
+    if(localStorage[calendar]!=null){
+        delete localStorage[calendar]
+        el.innerHTML='<li style="'+hyperlinkStyle('#2AB30E')+';font-size:'+SubMenuFontSize()+'">Reseted calendar with success!</li>'
+    }
+    else{
+        el.innerHTML='<li style="'+hyperlinkStyle('#497964')+';font-size:'+SubMenuFontSize()+'">Already at initial state!</li>'
+    }
+    
+    var f=function(element){
+        element.innerHTML='<li style="'+hyperlinkStyle('#0183D9')+';cursor:pointer;font-size:'+SubMenuFontSize()+'">Reset Calendar</li>'
+        updatePrevWidthHeight()
+    }
+    setTimeout(function(){f(el)},2000)
+    updatePrevWidthHeight()
+    //CreatePage()
+}
+function removeCalendar(el){
+    if(confirm("Are you sure?")){
+        
+        var calendar=el.parentElement.parentElement.innerText.split("\n")[0];
+        delete localStorage[calendar]
+        var aux=JSON.parse(localStorage["PCM"])
+        delete aux[calendar]
+        localStorage["PCM"]=JSON.stringify(aux)
+        all_calendars=aux
+        CreatePage()
+    }
+    
+}
+function importCalendarUndo(input){
+
+    input.parentElement.parentElement.outerHTML='<ul onclick="importCalendar(this)"><li style="'+hyperlinkStyle('#0183D9')+';font-size:'+SubMenuFontSize()+'">Import Calendar</li></ul>'
+    updatePrevWidthHeight()
+ 
+}
+function importCalendarActionBtn(input){
+    
+    
+    // Do something
+    //alert(document.getElementsByTagName("input")[0].value);
+    input=input.parentElement.children[0]
+    if(input.value!=""){
+        var _calendar=input.parentElement.parentElement.parentElement.parentElement.innerText.split("\n")[0];
+
+        localStorage[_calendar]=input.value
+        var ul=input.parentElement.parentElement
+        input.parentElement.parentElement.innerHTML='<li style="'+hyperlinkStyle('#2AB30E')+'">Imported calendar with success!</li>'
+        var f=function(ele){
+            ele.outerHTML='<ul onclick="importCalendar(this)"><li style="'+hyperlinkStyle('#0183D9')+';font-size:'+SubMenuFontSize()+'">Import Calendar</li></ul>'
+            updatePrevWidthHeight()
+        }
+
+        setTimeout(function(){f(ul)},2000)
+        updatePrevWidthHeight()
+    }
+    else{
+        input.placeholder="Must not be empty!"
+    }
+    
+}
+function importCalendarAction(event,input){
+    //press enter
+    if (event.keyCode == 13 ) {
+        // Do something
+        //alert(document.getElementsByTagName("input")[0].value);
+        if(input.value!=""){
+            var _calendar=input.parentElement.parentElement.parentElement.parentElement.innerText.split("\n")[0];
+            
+            localStorage[_calendar]=input.value
+            var ul=input.parentElement.parentElement
+            input.parentElement.parentElement.innerHTML='<li style="'+hyperlinkStyle('#2AB30E')+'">Imported calendar with success!</li>'
+            var f=function(ele){
+                ele.outerHTML='<ul onclick="importCalendar(this)"><li style="'+hyperlinkStyle('#0183D9')+'">Import Calendar</li></ul>'
+                updatePrevWidthHeight()
+            }
+
+            setTimeout(function(){f(ul)},2000)
+            updatePrevWidthHeight()
+        }
+        else{
+            input.placeholder="Must not be empty!"
+        }
+    }
+}
+function importCalendar(el){
+    
+    var calendar=el.parentElement.parentElement.innerText.split("\n")[0];
+
+    openInput(el,'Insert JSON','importCalendarAction(event,this)',okBtn('importCalendarActionBtn(this)'),crossBtn('importCalendarUndo(this)'))
+    updatePrevWidthHeight()
+}
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  
+    
+};
+function exportCalendar(el){
+    //need to put the json text in clipboard can see an example in site copy contact
+    //put setimeout to warning the user of the json in clipboard and change back to normal view.
+    var calendar=el.parentElement.parentElement.innerText.split("\n")[0];
+    if(localStorage[calendar]!=null){
+        var calendar_json=localStorage[calendar]
+        copyToClipboard(calendar_json)
+        el.innerHTML='<li style="'+hyperlinkStyle('#2AB30E')+';font-size:'+SubMenuFontSize()+'">Copy JSON to clip board!</li>'
+    }
+    else{
+        el.innerHTML='<li style="'+hyperlinkStyle('#497964')+';font-size:'+SubMenuFontSize()+'">Never entered in calendar!</li>'
+    }
+    
+    var f=function(ele){
+        ele.innerHTML='<li style="'+hyperlinkStyle('#0183D9')+';cursor:pointer;font-size:'+SubMenuFontSize()+'">Export Calendar</li>'
+        updatePrevWidthHeight()
+    }
+    
+    setTimeout(function(){f(el)},2000)
+    updatePrevWidthHeight()
+}
+
+
+function clickAction(el){
+    if(el.getAttribute("was_clicked")=="true"){
+        el.setAttribute("was_clicked","false")
+        removeSubMenu(el.parentElement)
+    }
+    else{
+        el.setAttribute("was_clicked","true")
+        addSubMenu(el.parentElement)
+    }
+    //update prev_width and height so that main does not createPage() again
+    updatePrevWidthHeight()
+}
+
+function CreateTableRow(string){
+    var row=document.createElement("tr")
+    var column=document.createElement("td")
+    var column2=document.createElement("td")
+    
+    row.appendChild(column)
+    row.appendChild(column2)
+    
+    var ul=document.createElement("ul")
+    var li=document.createElement("li")
+    ul.appendChild(li)
+    column.appendChild(ul)
+    
+    var text=document.createTextNode(string)
+    li.appendChild(text)
+    li.setAttribute("onmouseover","Highlight(this)")
+    li.setAttribute("onmouseout","NotHighLight(this)")
+    li.setAttribute("was_clicked","false")
+    li.setAttribute("onclick","clickAction(this)")
+    li.setAttribute("style","cursor:pointer")
+    
+    column2.innerHTML="<ul>"+all_calendars[string]+"</ul>"
+    
+    
+    
+    return row
+    
+    
+}
+String.prototype.replaceAll = function(search, replacement) {
+            var target = this;
+            return target.replace(new RegExp(search, 'g'), replacement);
+        };
+        function checkRoomName(){
+            var input=document.getElementById("roomName")
+            input.value=input.value.replaceAll(" ","-")
+        }
+function createPC(event,input){
+    if (event.keyCode == 13 ) {
+        // Do something
+        //alert(document.getElementsByTagName("input")[0].value);
+        if(input.value!=""){
+            input.value=input.value.replaceAll(" ","-")
+            all_calendars[input.value]="(0/365)"
+            localStorage["PCM"]=JSON.stringify(all_calendars)
+            setTimeout(CreatePage,500)
+        }
+        else{
+            input.placeholder="Must not be empty!"
+        }
+    }
+    
+}
+function createPCBtn(el){
+    var input=el.parentElement.children[0]
+    
+    if(input.value!=""){
+        input.value=input.value.replaceAll(" ","-")    
+        all_calendars[input.value]="(0/365)"
+        localStorage["PCM"]=JSON.stringify(all_calendars)
+        setTimeout(CreatePage,500)
+    }
+    else{
+        input.placeholder="Must not be empty!"
+    }
+}
+function undoCreatePC(el){
+    el.parentElement.parentElement.parentElement.innerHTML='<ul onclick="openInput(this,\'Insert PC Name\',\'createPC(event,this)\',okBtn(\'createPCBtn(this)\'),crossBtn(\'undoCreatePC(this)\'))"><li style="'+hyperlinkStyle('#0183D9')+'">+ create purpose calendar</li></ul>'
+}
+function okBtn(onclick_function){
+    return '<button onclick="'+onclick_function+'" style="border-radius:50%;background:green;color:white;font-size:'+inputFontSize()+';margin-left:2%;">&#160;&#10004&#160;</button>';
+}
+function crossBtn(onclick_function){
+    return '<button onclick="'+onclick_function+'" style="border-radius:50%;background:red;color:white;margin-left:2%;font-size:'+inputFontSize()+'">&#160;&#10006&#160;</button>'
+}
+function openInput(el,placeholder,on_key_press_function,okBtn_function,crossBtn_function){
+    var prev_html=el.innerHTML
+    el.setAttribute("onclick","")
+    el.innerHTML='<li><input onkeypress="'+on_key_press_function+'" type="text" placeholder="'+placeholder+'" style="width:70%;height:auto;font-size:'+inputFontSize()+';">'+okBtn_function+crossBtn_function+'</li>'
+    
+    //update prev_width and height so that main does not createPage() again
+    updatePrevWidthHeight()
+}
+
+function addNewPurposeCalendarRow(){
+    var row=document.createElement("tr")
+    var column=document.createElement("td")
+    row.appendChild(column)
+    column.setAttribute("colspan","2")
+    column.innerHTML='<ul onclick="openInput(this,\'Insert PC Name\',\'createPC(event,this)\',okBtn(\'createPCBtn(this)\'),crossBtn(\'undoCreatePC(this)\'))"><li style="'+hyperlinkStyle('#0183D9')+';cursor:pointer">+ create purpose calendar</li></ul>'
+    
+    return row
+    
+}
+function CreatePage(){
+    var title=document.getElementById("title")
+    title.style["font-size"]=titleSize()
+    
+    var table=document.getElementsByTagName("table")[0]
+    table.style["font-size"]=tableFontSize()
+    
+    table.style["padding-left"]=tablePadding()
+    
+    var tbody=document.getElementsByTagName("tbody")[0]
+    tbody.innerHTML="";
+    for(var key in all_calendars){
+        tbody.appendChild(CreateTableRow(key))    
+    }
+    //Add create new purpose calendar row
+    tbody.appendChild(addNewPurposeCalendarRow())
+    
+}
+
+function DetectinputBox(){
+    if(document.getElementsByTagName("input").length!=0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+//------Main------------
+
+window.onload=function(){
+    CreatePage()
+    
+    html=document.getElementsByTagName("html")[0];
+    global_width=html.offsetWidth;
+    global_height=html.offsetHeight;
+    prev_width=global_width;
+    prev_height=global_height;
+    requestAnimationFrame(Main)
+}
+
+
+function Main(){
+    var html=document.getElementsByTagName("html")[0];
+    
+    global_width=html.offsetWidth;
+    global_height=html.offsetHeight;
+    //console.log("with: "+global_width)
+    //console.log("height: "+global_height)
+    if((prev_width!=global_width || prev_height!=global_height)&& !DetectinputBox()){
+        console.log("change")
+        CreatePage()
+        prev_height=global_height
+        prev_width=global_width
+        
+        
+    }
+    requestAnimationFrame(Main)
+}
+
